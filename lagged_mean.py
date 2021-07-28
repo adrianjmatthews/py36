076c350f4613132a49760ce14356f3b2eeb50114
adrianjmatthews <adrian.j.matthews@gmail.com>
2021-07-28 16:37:38 +0100

    Added hard load (non lazy load) option to lagged_mean.py to speed up.

--- lagged_mean.py
+++ lagged_mean.py
@@ -1,10 +1,12 @@
 """Calculate time-lagged mean using data_analysis.TimeDomStats."""
 
+import datetime
 import os
 
 import iris
 import iris.quickplot as qplt
 import matplotlib.pyplot as plt
+import pdb
 
 import data_analysis as da
 
@@ -14,13 +16,12 @@ BASEDIR=os.path.join(os.path.sep,'gpfs','scratch','e058','data')
 ARCHIVE=True
 BASEDIR_ARCHIVE=os.path.join(os.path.sep,'gpfs','afm','matthews','data')
 
-#VAR_NAME='m_uwndprime_dvrtdxbar'; 
-LEVEL=850; SOURCE='erainterimEK2_plev_6h'; 
-#TDOMAINID='CCEK75E98-18-0.5-6h'
+VAR_NAME='uwnd'; LEVEL=1; SOURCE='era5mcw_sfc_h'; TDOMAINID='CCEK102E98-20-00UTC-and-M0002'
+#VAR_NAME='vrt'; LEVEL=850; SOURCE='erainterimEK3_plev_6h'; TDOMAINID='CCEK75E98-18-0.5-6h'
 #VAR_NAME='vrt'; LEVEL=850; SOURCE='ncepdoe_plev_d'; TDOMAINID='cckw75Epm10gt109812vrtsng'
 #VAR_NAME='vwndptap'; LEVEL=850; SOURCE='ncepncar_plev_d'; TDOMAINID='rmm001djf3'
 #VAR_NAME='olr'; LEVEL=0; SOURCE='olrinterp_toa_d'; TDOMAINID='rmm001djf3'
-#VAR_NAME='ppt'; LEVEL=1; SOURCE='trmm3b42v7p1_sfc_d'; TDOMAINID='CCEK75E98-18a-0.5-00UTC'
+#VAR_NAME='ppt'; LEVEL=1; SOURCE='trmm3b42v7p1_sfc_d'; TDOMAINID='CCEK102E98-18-00UTC'
 
 FILEPRE='' # e.g., '', '_rac', '_l30_n241'
 
@@ -32,7 +33,16 @@ DATA_FROM_ANNCYCLE=False
 #DATA_FROM_ANNCYCLE=(1998,2018)
 
 # lags are integer multiples of timedelta attribute, e.g., 6 hours for erainterim_plev_6h
-LAGS=[-4,0,4]
+#LAGS=[-1,0,1]
+LAGS=list(range(-24,24+1))
+
+# Set lazy load to false to force hard load of entire data set.
+# Memory hungry but can lead to significant speed up
+# Need to manually set first and last times to encompass all dates from time domain with lags
+LAZY_LOAD=False
+if not LAZY_LOAD:
+    TIME_FIRST=datetime.datetime(1998,1,1)
+    TIME_LAST=datetime.datetime(2020,12,31)
 
 VERBOSE=2
 
@@ -51,9 +61,13 @@ descriptor['archive']=ARCHIVE
 descriptor['basedir_archive']=BASEDIR_ARCHIVE
 descriptor['filepre']=FILEPRE
 descriptor['data_from_anncycle']=DATA_FROM_ANNCYCLE
+if 'TIME_FIRST' in locals():
+    descriptor['time_first']=TIME_FIRST
+if 'TIME_LAST' in locals():
+    descriptor['time_last']=TIME_LAST
 
 # Create instance of TimeDomStats object
-aa=da.TimeDomStats(**descriptor)
+aa=da.TimeDomStats(lazy_load=LAZY_LOAD,**descriptor)
 
 # Calculate event means and time mean
 aa.f_lagged_mean(lags=LAGS)
