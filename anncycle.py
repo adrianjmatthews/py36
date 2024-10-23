d698d4c0445a8f94d6057ffebe0e71956e5b86ea
adrianjmatthews <adrian.j.matthews@gmail.com>
2024-10-23 16:10:42 +0100

    Updated for Mohamed.

--- anncycle.py
+++ anncycle.py
@@ -18,14 +18,17 @@ BASEDIR_ARCHIVE=os.path.join(os.path.sep,'gpfs','afm','matthews','data')
 # YEAR1-YEAR2 are complete years over which to calculate annual cycle
 #VAR_NAME='div'; LEVEL=975; SOURCE='erainterim_plev_6h'; YEAR1=1998; YEAR2=2018
 #VAR_NAME='vwnd'; LEVEL=850; SOURCE='erainterim_plev_d'; YEAR1=1998; YEAR2=2018
+#VAR_NAME='div'; LEVEL=975; SOURCE='era5mcw_plev_h'; YEAR1=1998; YEAR2=2018
 #VAR_NAME='zg'; LEVEL=250; SOURCE='hadgem2esajhog_plev_d'; YEAR1=1985; YEAR2=1993
+VAR_NAME='uwnd'; LEVEL=250; SOURCE='igcm0001_plev_d'; YEAR1=3030; YEAR2=3034
 #VAR_NAME='zg'; LEVEL=200; SOURCE='ncepdoe_plev_d'; YEAR1=1979; YEAR2=1997
 #VAR_NAME='wndspd'; LEVEL=1; SOURCE='ncepncar_sfc_d'; YEAR1=1979; YEAR2=2015
 #VAR_NAME='uwnd'; LEVEL=850; SOURCE='ncepdoe_plev_d'; YEAR1=1998; YEAR2=2018
 #VAR_NAME='olr'; LEVEL=0; SOURCE='olrinterp_toa_d'; YEAR1=2010; YEAR2=2011
 #VAR_NAME='sst'; LEVEL=1; SOURCE='sstrey_sfc_d'; YEAR1=1982; YEAR2=2015
 #VAR_NAME='ppt'; LEVEL=1; SOURCE='trmm3b42v7p1_sfc_d'; YEAR1=1998; YEAR2=2020
-VAR_NAME='ucur'; LEVEL=0.494025; SOURCE='glorys12v1_zlev_d'; YEAR1=2005; YEAR2=2010
+#VAR_NAME='ppt'; LEVEL=1; SOURCE='imergv07btrmrgp1_sfc_d'; YEAR1=2001; YEAR2=2023
+#VAR_NAME='ucur'; LEVEL=0.494025; SOURCE='glorys12v1_zlev_d'; YEAR1=2005; YEAR2=2010
 
 # If TIME1 and TIME2 are False, anomalies from annual cycle are calculated
 # over whole input data set,
@@ -33,7 +36,7 @@ VAR_NAME='ucur'; LEVEL=0.494025; SOURCE='glorys12v1_zlev_d'; YEAR1=2005; YEAR2=2
 # If TIME1 is False and TIME2 is datetime, calculated from start to TIME2
 # If TIME1 and TIME2 are datetime, calculated from TIME1 to TIME2
 TIME1=TIME2=False
-#TIME1=cftime.DatetimeGregorian(2005,1,1); TIME2=False
+#TIME1=cftime.DatetimeGregorian(2021,1,1); TIME2=False
 #TIME1=False;  TIME2=cftime.DatetimeGregorian(1980,12,31)
 #TIME1=cftime.DatetimeGregorian(1998,1,1);  TIME2=cftime.DatetimeGregorian(1998,12,31)
 
@@ -41,7 +44,7 @@ TIME1=TIME2=False
 # data from higher frequency (e.g., 6h) data.
 # Set to False to switch off
 ANNCYCLE_SOURCE=False
-#ANNCYCLE_SOURCE='erainterim_plev_d'
+#ANNCYCLE_SOURCE='era5mcw_plev_d'
 
 NHARM=3
 if VAR_NAME=='ppt':
@@ -53,7 +56,7 @@ DETREND=False
 
 VERBOSE=2
 
-PLOT=False
+PLOT=True
 
 #------------------------------------------------------------------
 
@@ -83,17 +86,17 @@ if aa.detrend:
     #aa.f_read_trend()
 
 # Either create and save raw annual cycle or read in previously calculated one
-#aa.f_anncycle_raw()
+aa.f_anncycle_raw()
 #aa.f_read_anncycle_raw()
 
 # Either create and save smoothed annual cycle or read in previously calculated one
-#aa.f_anncycle_smooth()
-aa.f_read_anncycle_smooth()
+aa.f_anncycle_smooth()
+#aa.f_read_anncycle_smooth()
 
 # Create or read higher frequency smoothed annual cycle from daily annual cycle if needed
 if aa.anncycle_source:
-    aa.f_expand_anncycle_smooth()
-    #aa.f_read_expanded_anncycle_smooth()
+    #aa.f_expand_anncycle_smooth()
+    aa.f_read_expanded_anncycle_smooth()
     pass
 
 # Either create and save anomaly data (smooothed annual cycle subtracted),
@@ -102,9 +105,13 @@ aa.f_subtract_anncycle()
 #aa.f_read_subtract_anncycle()
 
 if PLOT:
+    fig=plt.figure()
+
     #timecon=da.set_time_constraint(cftime.DatetimeGregorian(1,1,16),False,calendar=aa.calendar,verbose=True)
-    timecon=da.set_time_constraint(cftime.DatetimeGregorian(2017,1,16),False,calendar=aa.calendar,verbose=True)
+    #timecon=da.set_time_constraint(cftime.DatetimeGregorian(2017,1,16),False,calendar=aa.calendar,verbose=True)
     #timecon=da.set_time_constraint(cftime.DatetimeGregorian(1980,1,1),cftime.DatetimeGregorian(2010,12,31))
+    #timecon=da.set_time_constraint(cftime.Datetime360Day(2101,7,1),False,calendar=aa.calendar,verbose=True)
+    timecon=da.set_time_constraint(cftime.Datetime360Day(3032,7,12),False,calendar=aa.calendar,verbose=True)
     latcon=iris.Constraint(latitude=0.)
     loncon=iris.Constraint(longitude=90.)
 
@@ -132,3 +139,6 @@ if PLOT:
     #qplt.plot(x2)
     
     plt.show()
+    imagefile='/gpfs/home/e058/tmp/fig1.png'
+    print('imagefile: {0!s}'.format(imagefile))
+    fig.savefig(imagefile)
